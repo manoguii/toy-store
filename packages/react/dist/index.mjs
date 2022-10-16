abc244dcba12f8a0f120658606c799adce229738
Guilherme David <guilhermedavidrk@icloud.com>
2022-10-16 16:55:43 -0300

    :zap: Create TextInput :bug: :fix:error: Text

--- packages/react/dist/index.mjs
+++ packages/react/dist/index.mjs
@@ -113,7 +113,7 @@ var Text = styled("p", {
   lineHeight: "$base",
   margin: 0,
   color: "$gray100",
-  variant: {
+  variants: {
     size: {
       xxs: { fontSize: "$xxs" },
       xs: { fontSize: "$xs" },
@@ -275,10 +275,68 @@ var Button = styled("button", {
     size: "md"
   }
 });
+
+// src/components/TextInput/styles.ts
+var TextInputContainer = styled("div", {
+  backgroundColor: "$gray900",
+  padding: "$3 $4",
+  borderRadius: "$sm",
+  boxSizing: "border-box",
+  border: "2px solid $gray900",
+  display: "flex",
+  alignItems: "baseline",
+  "&:has(input:focus)": {
+    borderColor: "$ignite300"
+  },
+  "&:has(input:disabled)": {
+    opacity: 0.5,
+    cursor: "not-allowed"
+  }
+});
+var Prefix = styled("span", {
+  fontFamily: "$default",
+  fontSize: "$sm",
+  color: "$gray400",
+  fontWeight: "regular"
+});
+var Input = styled("input", {
+  fontFamily: "$default",
+  fontSize: "$sm",
+  color: "$white",
+  fontWeight: "regular",
+  background: "transparent",
+  border: 0,
+  width: "100%",
+  "&:focus": {
+    outline: 0
+  },
+  "&:disabled": {
+    cursor: "not-allowed"
+  },
+  "&:placeholder": {
+    color: "$gray400"
+  }
+});
+
+// src/components/TextInput/index.tsx
+import { jsx as jsx2, jsxs as jsxs2 } from "react/jsx-runtime";
+function TextInput({ prefix, ...props }) {
+  return /* @__PURE__ */ jsxs2(TextInputContainer, {
+    children: [
+      !!prefix && /* @__PURE__ */ jsx2(Prefix, {
+        children: prefix
+      }),
+      /* @__PURE__ */ jsx2(Input, {
+        ...props
+      })
+    ]
+  });
+}
 export {
   Avatar2 as Avatar,
   Box,
   Button,
   Heading,
-  Text
+  Text,
+  TextInput
 };
