a03e5da70e2f293152cf4390cc7830e5871bc2a6
Guilherme David <guilhermedavidrk@icloud.com>
2022-10-16 16:02:26 -0300

    :zap: Create Avatar

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data Display/Avatar',
+  component: Avatar,
+
+  args: {
+    src: 'https://github.com/manoguii.png',
+    alt: 'Guilherme David',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
