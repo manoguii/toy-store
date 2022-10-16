ac1763426a2324f4ae48a82db8f1beb25afa3799
Guilherme David <guilhermedavidrk@icloud.com>
2022-10-16 20:11:50 -0300

    :zap: Add Improvements

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -9,6 +9,13 @@ export default {
     src: 'https://github.com/manoguii.png',
     alt: 'Guilherme David',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
